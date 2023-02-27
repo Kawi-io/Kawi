3442a92534ad4da8599e6c7306f2803e0a639fc9
Brayan Prieto Lozoya <brayan8prieto@hotmail.com>
2023-02-26 18:53:15 -0700

    wallet connected succesfully

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,20 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 
 import "~/styles/globals.css";
+import { ContextProvider } from '../components/ContextProvider';
+
+// Use require instead of import since order matters
+// require('antd/dist/antd.dark.less');
+require('@solana/wallet-adapter-ant-design/styles.css');
+require('@solana/wallet-adapter-react-ui/styles.css');
+require('../styles/globals.css');
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (<>
+    <ContextProvider>
+        <Component {...pageProps} />
+    </ContextProvider>
+  </>)
 };
 
 export default MyApp;
