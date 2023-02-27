750d8d3b82362b5996ddaadc106453d506c3328b
Sofía González <71847414+SofiaShaba@users.noreply.github.com>
2023-02-27 04:03:34 -0600

    <Who /> añadido a index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import { type NextPage } from "next";
 import { Landing } from '../components/Landing';
 import { What } from '../components/What';
+import { Who } from '../components/Who';
 
 const Home: NextPage = () => {
   return (
     <>
       <Landing />
       <What />
+      <Who />
     </>
   );
 };
