0bc646182636bb45eb41931e47a744f2fb012cc6
ArteaGaMer <luisarteagamer22@hotmail.com>
2023-02-27 02:37:57 -0600

    Boton de header arreglado

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -43,14 +43,7 @@ export const Header = () => {
         
 
           <div className="flex lg:hidden">
-            <button
-              type="button"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-              onClick={() => setMobileMenuOpen(true)}
-            >
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-            </button>
+          <WalletComponent />
           </div>
 
         
@@ -75,63 +68,6 @@ export const Header = () => {
       </nav>
 
       {/* Mobile menu */}
-
-
-        <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-          <div className="fixed inset-0 z-10" />
-          <Dialog.Panel
-            focus="true"
-            className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
-          >
-
-            {/* Logo */}
-            <div className="flex items-center justify-between">
-              <a href={logo.href} className="-m-1.5 p-1.5">
-                <span className="sr-only">{logo.name}</span>
-                <Image
-                  className="h-8 w-auto"
-                  src={logo.src}
-                  alt={`${logo.name} logo`}
-                />
-              </a>
-              <button
-                type="button"
-                className="-m-2.5 rounded-md p-2.5 text-gray-700"
-                onClick={() => setMobileMenuOpen(false)}
-              >
-                <span className="sr-only">Close menu</span>
-                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-              </button>
-            </div>
-
-            {/* Elements */}
-            <div className="mt-6 flow-root">
-              <div className="-my-6 divide-y divide-gray-500/10">
-                <div className="space-y-2 py-6">
-                  {headerElements.map((item) => (
-                    <a
-                      key={item.key}
-                      href={item.href}
-                      className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                    >
-                      {item.name}
-                    </a>
-                  ))}
-
-                </div>
-                {/* TODO: Solo mostrar si no hay una sesión iniciada */}
-                <div className="py-6">
-                  <Link
-                    href="/register/user"
-                    className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    Join
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </Dialog.Panel>
-        </Dialog>
 
       <hr className="border-1 border-black" />
     </header>
