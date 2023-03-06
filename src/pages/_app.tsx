cb45964bbd4b40a29bdec54be1c4c38a43ae8030
Sofía González <71847414+SofiaShaba@users.noreply.github.com>
2023-03-06 10:54:09 -0600

    Next ui instalado

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,29 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import { NextUIProvider } from "@nextui-org/react";
 
 import "~/styles/globals.css";
-import { ContextProvider } from '../components/ContextProvider';
-import { Header } from '../components/Header';
-import { Footer } from '../components/Footer';
+import { ContextProvider } from "../components/ContextProvider";
+import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
 
 // Use require instead of import since order matters
 // require('antd/dist/antd.dark.less');
-require('@solana/wallet-adapter-ant-design/styles.css');
-require('@solana/wallet-adapter-react-ui/styles.css');
-require('../styles/globals.css');
+require("@solana/wallet-adapter-ant-design/styles.css");
+require("@solana/wallet-adapter-react-ui/styles.css");
+require("../styles/globals.css");
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return (<>
-    
-    <ContextProvider>
-    <Header />
-        <Component {...pageProps} />
-    <Footer />
-    </ContextProvider>
-  </>)
+  return (
+    <>
+      <NextUIProvider>
+        <ContextProvider>
+          <Header />
+          <Component {...pageProps} />
+          <Footer />
+        </ContextProvider>
+      </NextUIProvider>
+    </>
+  );
 };
 
 export default MyApp;
