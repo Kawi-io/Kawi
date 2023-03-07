7f61e54fc44b4d1a86e4034c17c55401325be87c
Sofía González <71847414+SofiaShaba@users.noreply.github.com>
2023-03-06 23:45:08 -0600

    Tarjeta NFT para grid de dashboard

---
+++ src/components/NftCard.tsx
@@ -0,0 +1,56 @@
+import { Card, Col, Row, Button, Text } from '@nextui-org/react';
+
+export const NftCard = ({ title, image, description }: any) => {
+  return (
+    <Card css={{ w: "100%", h: "400px" }}>
+      <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+        <Col>
+          <Text h3 color="black">
+            {title}
+          </Text>
+        </Col>
+      </Card.Header>
+      <Card.Body css={{ p: 0 }}>
+        <Card.Image
+          src={image}
+          width="100%"
+          height="100%"
+          objectFit="cover"
+          alt="Card example background"
+        />
+      </Card.Body>
+      <Card.Footer
+        isBlurred
+        css={{
+          position: "absolute",
+          bgBlur: "#ffffff66",
+          borderTop: "$borderWeights$light solid rgba(255, 255, 255, 0.2)",
+          bottom: 0,
+          zIndex: 1,
+        }}
+      >
+        <Row>
+          <Col>
+            <Text color="#000" size={12}>
+              {description}
+            </Text>
+          </Col>
+          <Col>
+            <Row justify="flex-end">
+              <Button bordered auto rounded color="secondary">
+                <Text
+                  css={{ color: "inherit" }}
+                  size={12}
+                  weight="bold"
+                  transform="uppercase"
+                >
+                  Transfer
+                </Text>
+              </Button>
+            </Row>
+          </Col>
+        </Row>
+      </Card.Footer>
+    </Card>
+  );
+};
