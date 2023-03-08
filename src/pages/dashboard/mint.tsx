1ec48155f88b9de2bbf7a562dc7a40d0a08659d7
Sofía González <71847414+SofiaShaba@users.noreply.github.com>
2023-03-07 21:48:04 -0600

    Mint update

--- src/pages/dashboard/mint.tsx
+++ src/pages/dashboard/mint.tsx
@@ -1,29 +1,39 @@
 /* eslint-disable */
 import { type NextPage } from "next";
 import Head from "next/head";
-
-import Image from "next/image";
-
+import { useWallet } from "@solana/wallet-adapter-react";
 import { useEffect, useState } from "react";
-
 import { PublicKey, Connection, clusterApiUrl } from "@solana/web3.js";
-
 import { Metaplex, walletAdapterIdentity } from "@metaplex-foundation/js";
 
-import { getCandyMachineState, mint } from "../../components/CandyMachine";
+import { getCandyMachineState, mint, NftCard } from "../../components/index";
+import { Container, Grid } from "@nextui-org/react";
 
-import { useWallet } from "@solana/wallet-adapter-react";
+const nftsTest = [
+  {
+    name: "1 year with Kawi",
+    description: "You have been working wit Kawi for a year, congrats",
+    image:
+      "https://media.giphy.com/media/h2VRjenjc8ly5JGUN0/giphy-downsized-large.gif",
+    symbol: "",
+  },
+  {
+    name: "1 year with Kawi",
+    description: "You have been working wit Kawi for a year, congrats",
+    image:
+      "https://media.giphy.com/media/h2VRjenjc8ly5JGUN0/giphy-downsized-large.gif",
+    symbol: "",
+  },
+];
 
 const Mint: NextPage = () => {
+  const [walletAvailable, SetWalletAvailable] = useState(false);
+  const [candyMachineState, SetCandyMachineState] = useState<any>();
+  const { wallet } = useWallet();
+
   const connection = new Connection(clusterApiUrl("devnet"));
 
   const metaplex = new Metaplex(connection);
-
-  const { wallet } = useWallet();
-
-  const [walletAvailable, SetWalletAvailable] = useState(false);
-
-  const [candyMachineState, SetCandyMachineState] = useState<any>();
 
   const candyMachineId = new PublicKey(
     "F9Z379ypQZmQB9Mige7eBqYyNdNRVYW5hg6Nog56xE1S"
@@ -34,9 +44,10 @@ const Mint: NextPage = () => {
   );
 
   const [nftDemo, SetNfteDemo] = useState({
-    name: "",
-    description: "",
-    image: "https://media4.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif",
+    name: "1 year with Kawi",
+    description: "You have been working wit Kawi for a year, congrats",
+    image:
+      "https://media.giphy.com/media/h2VRjenjc8ly5JGUN0/giphy-downsized-large.gif",
     symbol: "",
   });
 
@@ -74,85 +85,44 @@ const Mint: NextPage = () => {
     }
   };
 
+  // Esta accion se llama al hacer click en mint, solo hay que modificarla
+  const mintTest = () => {
+    console.log("HOLA");
+  };
+
   // const captureKey = (e:any) => {
   //     SetCandyMachineAuthority(new PublicKey(e))
   // }
   return (
     <>
-      <title>Mint a new certificate</title>
+      <Head>
+        <title>Mint a new certificate</title>
+      </Head>
 
-      <div className="py-10 px-8 sm:px-40">
-        <h1 className="text-center px-4 sm:px-0">
-          Create some <span className="text-purple">NFTS:</span>
-        </h1>
-      </div>
-
-      <main className="bg-white">
-        <div className="text-center max-w-7xl mx-auto py-6 sm:px-6 sm:px-6 lg:px-8">
-          <div>
-            <h3>
-              {nftDemo.name}: {candyMachineState?.itemsRemaining} remaining
-            </h3>
-          </div>
-          <div className="mt-4">
-            {/* este hijo de perra deberia ser blanco y no se por que no se pone blanco */}
-            <p className="blanco">{nftDemo.description}</p>
-          </div>
-          <div className="flex justify-center my-4">
-            <Image
-              src={nftDemo.image}
-              alt={nftDemo.name}
-              width={100}
-              height={100}
-            />
-          </div>
-          <div className="flex justify-center items-center">
-            {/* <div className="mt-5 col-span-6 sm:col-span-3">
-              <label
-                htmlFor="wallet"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Auth
-              </label>
-              <input
-                type="text"
-                name="wallet"
-                id="wallet"
-                placeholder="Your wallet"
-                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-teal-500 focus:ring-teal-500 sm:text-sm"
-                required
-              />
-            </div> */}
-            <button
-              className="
-              inline-flex
-              items-center
-              rounded-full
-              px-10
-              py-3
-              text-sm
-              font-medium
-              shadow-sm
-              focus:outline-none 
-              focus:ring-2 
-              focus:ring-offset-2
-              bg-purple
-              hover:bg-purple-900
-              focus:ring-purple-500
-              text-white
-              border-transparent
-              w-3/5
-              flex justify-center
-              "
-              title="mint"
-              onClick={() => doMint()}
-              disabled={!walletAvailable}
-            >
-              Mint !
-            </button>
-          </div>
+      <Container className="p-3">
+        <div className="py-10 px-8 sm:px-40">
+          <h1 className="text-center px-4 sm:px-0 sm:text-5xl">
+            Mint a new <span className="text-purple">certificate</span>
+          </h1>
         </div>
-      </main>
+        <div className="my-3">
+          <hr className="border-1 h-0.5 bg-black" />
+        </div>
+
+        <Grid.Container gap={2} justify="center">
+          {nftsTest.map((nft) => (
+            <Grid lg={3} sm={4}>
+              <NftCard
+                title={nft.name}
+                image={nft.image}
+                description={nft.description}
+                event={mintTest}
+                btnText="Mint"
+              />
+            </Grid>
+          ))}
+        </Grid.Container>
+      </Container>
     </>
   );
 };
