b1ab546dc0d6cf7e897544ba9a9094348e7b8ef1
Sofía González <71847414+SofiaShaba@users.noreply.github.com>
2023-03-09 23:40:27 -0600

    Register nuevo campo y modal de error

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -6,6 +6,9 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { Container } from "@nextui-org/react";
 
+import { CustomModal } from "../components/index";
+import Head from "next/head";
+
 const user_fields = [
   {
     label: "Name",
@@ -70,9 +73,14 @@ const company_fields = [
 const Register: NextPage = () => {
   const [isCompany, setIsCompany] = useState(false);
   const router = useRouter();
+  const [modal, setModal] = useState({
+    visible: false,
+    title: "",
+    text: "",
+  });
   const [formData, setFormData] = useState({
-    _id: "", // TODO: Wallet id here, aun no funcinal
-    populationReg: "",
+    _id: "",
+    population_registry: "",
     about: "",
     name: "",
     profession: "",
@@ -113,19 +121,24 @@ const Register: NextPage = () => {
       })
         .then((response) => response.json())
         .then((data) => {
-          event.target.reset();
-
           if (data.acknowledged && publicKey) {
+            event.target.reset();
             sessionStorage.setItem("publicKey", publicKey?.toBase58());
             router.push(`/profile/${publicKey}`);
-          } else alert("El usuario ya fue registrado");
+          } else {
+            setModal({
+              ...modal,
+              visible: true,
+              title: "Error",
+              text: "El usuario ya esta registrado",
+            });
+          }
         })
-        .catch((error) => alert("El usuario ya fue registrado"));
+        .catch((error) => setModal({ ...modal, visible: true, title: "Error", text: error }));
 
       return;
     }
-
-    alert("Please connect your wallet first");
+    setModal({...modal, visible: true, title: "Error", text: "Please connect your wallet to proceed"})
   };
 
   // Manejo de cambios en los campos del form
@@ -140,6 +153,17 @@ const Register: NextPage = () => {
 
   return (
     <>
+      <Head>
+        <title>Register</title>
+      </Head>
+
+      <CustomModal
+        visible={modal.visible}
+        title={modal.title}
+        text={modal.text}
+        close={() => setModal({ ...modal, visible: false })}
+      />
+
       <Container className="p-3">
         <div className="py-5 px-8 sm:px-40">
           <h1 className="text-center px-4 sm:px-0 sm:text-5xl">
@@ -163,19 +187,17 @@ const Register: NextPage = () => {
 
           <form onSubmit={handleFormSubmit} className="mt-5">
             <div>
-              <div
-                className="mt-5 col-span-6 sm:col-span-3"
-              >
+              <div className="mt-5 col-span-6 sm:col-span-3">
                 <label
-                  htmlFor="populationReg"
+                  htmlFor="population_registry"
                   className="block text-sm font-medium text-gray-700"
                 >
                   Population Registry
                 </label>
                 <input
                   type="text"
-                  name="populationReg"
-                  id="populationReg"
+                  name="population_registry"
+                  id="population_registry"
                   placeholder="Your country's population registry"
                   className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-teal-500 focus:ring-teal-500 sm:text-sm"
                   onChange={handleInputChange}
